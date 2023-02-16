af017a5a1bff3ce1c6bac88cbf4fa19c97430e40
horbital1140 <adeemma1993@gmail.com>
2023-02-16 13:53:30 -0800

    rttyuk

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - a line that printf function
- * return: 0 (succes)
+ * main - program that print function
+ * Return: 0 sucess
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-
