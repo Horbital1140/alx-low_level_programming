9c6bcce8b6d7caeec0b8453b08a1d2f1a11082d2
horbital1140 <adeemma1993@gmail.com>
2023-02-17 11:53:59 -0800

    dfgb

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - a program that print zero to nine
+ * Description: print single digit numbers
  * Requiire: 0
  */
 int main(void)
 {
-	printf("0123456789\n");
+	putchar("0123456789\n");
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a program that print single number
+ * Require: 0 (success)
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
