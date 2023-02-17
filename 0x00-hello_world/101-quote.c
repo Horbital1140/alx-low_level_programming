5ff488d6138890365c20d96cae65f76a86a03e00
horbital1140 <adeemma1993@gmail.com>
2023-02-16 21:11:12 -0800

    fvbhn

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a c program thet print exactly new line
+ * return: 1 success
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
+	return (1);
+}
