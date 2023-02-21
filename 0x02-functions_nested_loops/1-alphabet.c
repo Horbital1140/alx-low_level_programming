ca3dea7048a2ce8fcc8193b14162fc5d69ffb34c
horbital1140 <adeemma1993@gmail.com>
2023-02-21 11:51:45 -0800

    dfghj

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - alphabet in lowercase lettee
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z', c++)
+	{
+		_putchar(c)
+	}
+	_putchar('\n');
+	return (0);
+}
