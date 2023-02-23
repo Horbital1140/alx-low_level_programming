b2bc729e6c883710d2ff6a842ec20ba5505f3e1a
horbital1140 <adeemma1993@gmail.com>
2023-02-23 09:51:13 -0800

    bnm

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * _isupper- determine if a character is upper
+ *@c: accepts an integer
+ * Description: program will assign a random number to the variable n each time
+ * it is executed.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isupper(int c)
+{
+	if(_isupper(c))
+		return(1);
+		return(0);
+
+}
