761e1e2bfa70eed9618f9de7f182d6eff6f937a0
horbital1140 <adeemma1993@gmail.com>
2023-05-04 14:56:28 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * clear_bit - change_number to binary
+ * @n: numb_change
+ * @index: numb_to_change
+ *
+ * Return:int
+ */
+
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	/* this code Check the index validity */
+	if (index >= (sizeof(unsigned long int) * 5))
+		return (-1);
+
+	/* below line of code Clear the bit at index by shifting 1 to the left and negating */
+	*n &= ~(1ul << index);
+
+	return (1);
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - change_number to binary
+ * @n: numb_change
+ * @m: numb_to_change
+ *
+ * Return:int
+ */
+
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor_output = n ^ m;
+	unsigned int note = 0;
+
+	while (xor_output)
+	{
+		if (xor_output & 1)
+			note++;
+		xor_output >>= 1;
+	}
+
+	return (count);
+}
