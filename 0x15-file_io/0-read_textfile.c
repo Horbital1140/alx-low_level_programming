3317cd8fe8251a4c31770c43e9de438ade6c8551
horbital1140 <adeemma1993@gmail.com>
2023-05-08 15:18:25 -0700

    file i/o 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,42 +1,54 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
 
 /**
- * read_textfile - text file will be read, it will be printed in posix stdio.
- *  @filename: it point to the file
- * @letters: Total number of letters the
- *           function should read_print.
+ * read_textfile - reads_text_file and prints it to the POSIX stdout
+ * @filename: pointer_name of file_to read
+ * @letters: number of letters to read_and_print
  *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ * Return: actual number of letters_read_and printed; or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t a;
-	ssize_t b;
-	ssize_t c;
+	int fil_e;
+	ssize_t r_bytes, w_bytes;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	fil_e = open(filename, O_RDONLY);
+	if (fil_e == -1)
 		return (0);
 
-	a = open(filename, O_RDONLY);
-	b = read(a, buffer, letters);
-	c = write(STDOUT_FILENO, buffer, b);
-
-	if (a == -1 || b == -1 || c == -1 || c != b)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
+		close(fil_e);
+		return (0);
+	}
+
+	r_bytes = read(fil_e, buffer, letters);
+	if (r_bytes == -1)
+	{
+		close(fil_e);
 		free(buffer);
 		return (0);
 	}
 
+	w_bytes = write(STDOUT_FILENO, buffer, r_bytes);
+	if (w_bytes == -1 || (size_t) w_bytes != (size_t) r_bytes)
+	{
+		close(fil_e);
+		free(buffer);
+		return (0);
+	}
+
+	close(fil_e);
 	free(buffer);
-	close(a);
 
-	return (c);
+	return (r_bytes);
 }
-
